aa9315acfeb1ea179d5d9fa7bde78eabaf98a8dc
Sanjaeyss <sanjaeyss@gmail.com>
2024-08-29 00:28:53 -0400

    Update read.py

--- tests/read.py
+++ tests/read.py
@@ -1,1 +1,11 @@
+import json
+
+
+with open("coverage.json", "r") as f:
+    coverage_data = json.load(f)
+
+
+total_coverage = coverage_data['totals']['percent_covered']
+
+print(f"Total Coverage: {total_coverage}%")
 
