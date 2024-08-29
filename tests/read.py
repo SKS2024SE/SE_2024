89bb412452ed03c2a2f0376d242bfc705f0e662d
Sanjaeyss <sanjaeyss@gmail.com>
2024-08-29 00:31:53 -0400

    Create read.py
    A python program to read coverage.json file

---
+++ tests/read.py
@@ -0,0 +1,10 @@
+import json
+
+
+with open("coverage.json", "r") as f:
+    coverage_data = json.load(f)
+
+
+total_coverage = coverage_data['totals']['percent_covered']
+
+print(f"Total Coverage: {total_coverage}%")
