ea039af7eeb5753e5e398741e9a457d0db3886e2
Sanjaeyss <sanjaeyss@gmail.com>
2024-09-12 05:34:05 +0000

    Testing the working of tools by commiting

--- src/HW2/test_tools.py
+++ src/HW2/test_tools.py
@@ -1,1 +1,9 @@
+def example_function():
+    x= 10
+    y =    20
+    if(x >5): print( 'X is greater than 5' )
+    else: print ('X is less than or equal to 5')
+    return    x+y
+
+example_function( )
 
