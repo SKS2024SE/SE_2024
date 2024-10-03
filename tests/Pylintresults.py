16878d9a2341fd5cfaa91a02b193f83baa5d572d
SandhiyaS24 <sandhiya2472@gmail.com>
2024-10-03 02:49:15 +0000

    Adding the Testing badges

--- tests/Pylintresults.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-import re
- 
- 
- pylint_results = os.path.abspath('/workspaces/SE_2024/post_traces/pylint_results.txt')
- /workspaces/SE_2024/post_traces/pylint_results.txt
- post_traces/pylint_results.txt
